c866bb5370695f93b87333c00a8af28cc8f537a8
Romuald du Song <romuald.dusong@deveryware.com>
2013-10-13 15:15:03 +0200

    update puppetlabs/apt usage

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class mariadb (
   # https://github.com/example42/puppi/issues/60
-  $version = hiera( 'mysql_version', '' )
+  $version = hiera( 'mysql_version', '' ),
+  $package = hiera( 'mysql_package', '' )
 ) {
 
   if ( ( $mariadb::version == '' ) or ( $mariadb::version == undef ) ) {
@@ -8,29 +9,27 @@ class mariadb (
   }
 
   $distro_lc  = inline_template("<%= operatingsystem.downcase %>")
-  $repo_version = inline_template("<%=@version.to_s.match(/\d+.\d+/)[0] %>")
+  $repo_version = inline_template('<%=@version.to_s.match(/\d+.\d+/)[0] %>')
   $distro_url = "http://mirrors.supportex.net/mariadb/repo/${repo_version}/${distro_lc}"
-  $mysql_package = hiera('mysql_package', $mysql::package)
 
   if ( ( $::operatingsystem != 'debian' ) and ( $::operatingsystem != 'ubuntu' ) ) {
     fail( "Distro ${::operatingsystem} not supported." )
   }
 
-  apt::repository { "mariadb":
-    url       => $distro_url,
-    distro    => $::lsbdistcodename,
-    repository=> 'main',
-    key       => 'dbart@askmonty.org',
-    key_url   => '"http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0xCBCB082A1BB943DB"',
+  apt::source { "mariadb":
+    location    => $distro_url,
+    release     => $::lsbdistcodename,
+    repos       => 'main',
+    key         => 'dbart@askmonty.org',
+    key_source  => 'http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0xCBCB082A1BB943DB',
  }
 
   exec {'mariadb_aptgetupdate':
-    command   => 'apt-get update',
-    onlyif    => "/bin/bash -c 'x=\$(apt-cache madison $mysql_package | \
+    command   => '/usr/bin/apt-get update',
+    onlyif    => "/bin/bash -c 'x=\$(apt-cache madison $mariadb::package | \
                   grep \"${distro_url}\" | wc -l); test \"\$x\" = \"0\" -a \"\$x\" != \"\" '"
   }
 
-  Apt::Repository['mariadb'] -> Exec['mariadb_aptgetupdate'] -> Package['mysql']
-
+  Apt::Source['mariadb'] -> Exec['mariadb_aptgetupdate'] -> Package['mysql']
 
 }
